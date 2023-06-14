3680cd94a925115da6d0eaaceea9e5f5ffc8fd56
xXAUNTHXx <kreeragid21.05.46@gmail.com>
2023-06-14 14:13:05 +0700

    add index

---
+++ pages/dashboard/index.js
@@ -0,0 +1,77 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function Component() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const handleOpneUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
+  const { data: session } = useSession()
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          {session
+          ? 
+          <div>
+              <IconButton onClick={handleOpneUserMenu} sx={{ p: 0 }}>
+                <Avatar alt={session.user.email} src={session.user.avatar} />
+              </IconButton>
+              <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <MenuItem  onClick={handleCloseUserMenu}>
+                  <Typography onClick={() => signOut()} textAlign="center">
+                    Logout
+                  </Typography>
+                </MenuItem>
+            </Menu>
+          </div>
+          : <Button onClick={() => signIn()}color="inherit">Login</Button>}
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </div>
+  );
+}
